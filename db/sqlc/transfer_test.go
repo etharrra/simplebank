696a4954d1681c2c3c15244d6766b5c1e709916c
etharrra <iterdare.2977@gmail.com>
2024-10-03 13:54:41 +0630

    test finished

---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,78 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/etharra/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomTransfer(t *testing.T, fromAccount Account, toAccount Account) Transfer {
+	arg := CreateTransferParams{
+		FromAccountID: fromAccount.ID,
+		ToAccountID:   toAccount.ID,
+		Amount:        util.RandomMoney(),
+	}
+
+	transfer, err := testQuries.CreateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, transfer.FromAccountID, arg.FromAccountID)
+	require.Equal(t, transfer.ToAccountID, arg.ToAccountID)
+	require.Equal(t, transfer.Amount, arg.Amount)
+
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	fromAccount := createRandomAccount(t)
+	toAccount := createRandomAccount(t)
+	createRandomTransfer(t, fromAccount, toAccount)
+}
+
+func TestGetTransfer(t *testing.T) {
+	fromAccount := createRandomAccount(t)
+	toAccount := createRandomAccount(t)
+	transfer1 := createRandomTransfer(t, fromAccount, toAccount)
+
+	transfer2, err := testQuries.GetTransfer(context.Background(), transfer1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer2)
+
+	require.Equal(t, transfer1.ID, transfer2.ID)
+	require.Equal(t, transfer1.FromAccountID, transfer2.FromAccountID)
+	require.Equal(t, transfer1.ToAccountID, transfer2.ToAccountID)
+	require.Equal(t, transfer1.Amount, transfer2.Amount)
+	require.WithinDuration(t, transfer1.CreatedAt, transfer2.CreatedAt, time.Second)
+}
+
+func TestListTransfers(t *testing.T) {
+	fromAccount := createRandomAccount(t)
+	toAccount := createRandomAccount(t)
+	for i := 0; i < 10; i++ {
+		createRandomTransfer(t, fromAccount, toAccount)
+		createRandomTransfer(t, toAccount, fromAccount)
+	}
+
+	arg := ListTransfersParams{
+		FromAccountID: fromAccount.ID,
+		ToAccountID:   toAccount.ID,
+		Limit:         5,
+		Offset:        5,
+	}
+
+	transfers, err := testQuries.ListTransfers(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfers)
+
+	for _, transfer := range transfers {
+		require.NotEmpty(t, transfer)
+		require.True(t, transfer.FromAccountID == fromAccount.ID || transfer.ToAccountID == toAccount.ID)
+	}
+}
---
+++ util/random.go
@@ -0,0 +1,76 @@
+package util
+
+import (
+	"math/rand"
+    "time"
+	"strings"
+)
+
+const alphabet = "abcefghijklmnopqrstuvwxyz"
+
+var randomSource *rand.Rand
+
+/**
+Initializes the random source using the current time as a seed value.
+*/
+func init() {
+    randomSource = rand.New(rand.NewSource(time.Now().UnixNano()))
+}
+
+/**
+ * RandomInt generates a random integer between the specified minimum and maximum values (inclusive).
+ * 
+ * @param min The minimum value for the random integer generation.
+ * @param max The maximum value for the random integer generation.
+ * @return int64 A random integer within the specified range.
+ */
+func RandomInt(min, max int64) int64 {
+	return min + randomSource.Int63n(max-min+1)
+}
+
+/**
+ * RandomString generates a random string of length n using characters from the alphabet.
+ *
+ * @param n The length of the random string to generate
+ * @return The randomly generated string
+ */
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[randomSource.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+/**
+ * RandomOwner generates a random string of length 6.
+ * 
+ * @return string: A randomly generated string of length 6.
+ */
+func RandomOwner() string  {
+	return RandomString(6)
+}
+
+/**
+ * RandomMoney generates a random integer representing a monetary value between 0 and 1000.
+ * 
+ * @return int64 - A randomly generated monetary value.
+ */
+func RandomMoney() int64  {
+	return RandomInt(0, 1000)
+}
+
+/**
+* RandomCurrency generates a random currency code from a predefined list of currencies.
+* 
+* @return string: A randomly selected currency code from the list of currencies.
+*/
+func RandomCurrency() string  {
+	currencies := []string{"EUR", "USD", "CAD"}
+	n := len(currencies)
+	return currencies[randomSource.Intn(n)]
+}
